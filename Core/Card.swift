709cc734ed1da72c32a191f6d6cb3562e96611fa
Marcus Bamberger <marcusbamberger@Marcuss-MacBook-Pro.local>
2019-10-21 12:02:11 -0700

    Added symbol requirement to cards

--- Core/Card.swift
+++ Core/Card.swift
@@ -36,17 +36,17 @@ enum Card: CaseIterable {
     // For concision, we store all the card properties in a single dictionary and
     // access them with helper functions.
     //------------------------------------------------------------------------------
-    static let values: [Card: (name: String, score: Int, image: String, color: Color)] = [
-        Bun:    ("Bun",         1,      "bunImage",         .yellow),
-        Sauce:  ("Sauce",       1,      "sauceImage",       .green),
-        Fries:  ("Fries",       2,      "friesImage",       .pink),
-        Drink:  ("Drink",       3,      "drinkImage",       .purple),
-        Vegetable:("Vegetable", 3,      "vegetableImage",   .gray),
-        Cheese: ("Cheese",      4,      "cheeseImage",      .orange),
-        Bird:   ("Bird",        5,      "birdImage",        .yellow),
-        Fish:   ("Fish",        5,      "fishImage",        .blue),
-        Cow:    ("Cow",         5,      "cowImage",         .red),
-        Pie:    ("Pie",         6,      "pieImage",         .purple),
+    static let values: [Card: (name: String, score: Int, image: String, color: Color, symbol: String)] = [
+        Bun:    ("Bun",         1,      "bunImage",         .yellow,    "bunSymbol"),
+        Sauce:  ("Sauce",       1,      "sauceImage",       .green,     "sauceSymbol"),
+        Fries:  ("Fries",       2,      "friesImage",       .pink,      "friesSymbol"),
+        Drink:  ("Drink",       3,      "drinkImage",       .purple,    "drinkSymbol"),
+        Vegetable:("Vegetable", 3,      "vegetableImage",   .gray,      "vegetableSymbol"),
+        Cheese: ("Cheese",      4,      "cheeseImage",      .orange,    "cheeseSymbol"),
+        Bird:   ("Bird",        5,      "birdImage",        .yellow,    "birdSymbol"),
+        Fish:   ("Fish",        5,      "fishImage",        .blue,      "fishSymbol"),
+        Cow:    ("Cow",         5,      "cowImage",         .red,       "cowSymbol"),
+        Pie:    ("Pie",         6,      "pieImage",         .purple,    "pieSymbol"),
     ]
     
     var name: String { Card.values[self]!.name }
