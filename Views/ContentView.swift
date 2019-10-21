5374088701272f8a46f664790e80c56b6006598a
Matt Bamberger <mattb@intelligentartifice.com>
2019-10-21 15:29:39 -0700

    Improve DeckView

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -15,35 +15,18 @@ import SwiftUI
 //------------------------------------------------------------------------------
 struct ContentView: View {
     var body: some View {
-        /*
-        let deck = Deck([.Cow: 4, .Fish: 3, .Bun: 2])
-        let player1Deck = Deck()
-        let player2Deck = Deck()
-        let player3Deck = Deck()
- */
         let player1Deck = Deck([.Cow: 3, .Fish: 2, .Bun: 1, .Sauce: 3, .Fries: 1])
-        //deck.shuffleAndDeal(recipients: [player1Deck, player2Deck, player3Deck])
         
         let currentOrder = CurrentOrder(originalOrder: MenuItem(name: "Fur Piece", ingredients: [.Bun:3, .Cow:2, .Pie:1]))
-        print(currentOrder.content)
         return ZStack {
-            /*
-            CardView(card: player1Deck.aCard())
-                .position(x: 220, y: 400)
-            
-            CardView(card: player2Deck.aCard())
-                .position(x: 520, y: 400)
-            
-            CardView(card: player3Deck.aCard())
-                .position(x: 820, y: 400)
-            */
             DeckView(player1Deck)
-                .position(x: 400, y: 200)
+                .position(x: 800, y: 1020)
 
             OrderView(currentOrder.content)
                 .position(x: 400, y: 50)
         }
         .frame(width: 1600, height: 1200)
+        .background(Color.gray)
     }
 }
 
