c7ebef210b3aed04b4df1f7bfd714a3769caec31
Marcus Bamberger <marcusbamberger@Marcuss-MacBook-Pro.local>
2019-10-22 13:53:34 -0700

    Re-added second restaurant

--- Core/Restaurant.swift
+++ Core/Restaurant.swift
@@ -109,4 +109,77 @@ Restaurant(
     smallDeck: Deck(
         cardCounts: [.Bun:9, .Sauce:4, .Fries:3, .Drink:6, .Vegetable:6, .Cheese:6, .Cow:5, .Fish:5, .Bird:5, .Pie:3]),
     largeDeck: Deck(
-        cardCounts: [.Bun:11, .Sauce:6, .Fries:5, .Drink:9, .Vegetable:8, .Cheese:8, .Cow:7, .Fish:6, .Bird:7, .Pie:4]))]
+        cardCounts: [.Bun:11, .Sauce:6, .Fries:5, .Drink:9, .Vegetable:8, .Cheese:8, .Cow:7, .Fish:6, .Bird:7, .Pie:4])),
+
+    Restaurant(
+    image:      "Friedey's zombie",
+    name:       "Friedey's Holiday Menu",
+    subTitle:   "Friedey's Celebrates the Festival of the Season",
+    menuCategories: [
+        MenuCategory(
+            name: "Stuffers",
+            range: 1...2,
+            menuItems: [
+                MenuItem(name: "David Copperfield", ingredients: [.Cheese:1]),
+                MenuItem(name: "Nicholar Nickleby", ingredients:
+                    [.Fries:1]),
+                MenuItem(name: "Little Match Girl", ingredients:
+                    [.Bird:2]),
+                MenuItem(name: "Oliver Twist", ingredients:
+                    [.Bun:1]),
+                MenuItem(name: "Wassail, Wassail", ingredients:
+                    [.Drink:2]),
+                MenuItem(name: "Carillon", ingredients:
+                    [.Bun:4])]),
+        MenuCategory(
+            name: "Mainstays",
+            range: 3...4,
+            menuItems: [
+                MenuItem(name: "Donner on Blitzen", ingredients:
+                    [.Bun:1, .Cow:2]),
+                MenuItem(name: "Friendly Beasts", ingredients:
+                    [.Cow:1, .Fish: 1, .Bird: 1]),
+                MenuItem(name: "Big Bowl o' Jelly", ingredients:
+                    [.Drink:1, .Cheese:1, .Fish:1]),
+                MenuItem(name: "Loaves and Fishes", ingredients:
+                    [.Bun:2, .Fish:2]),
+                MenuItem(name: "Jacob Marley", ingredients:
+                    [.Bun:1, .Fries:2, .Cheese:1]),
+                MenuItem(name: "Ebenezer Scrooge", ingredients:
+                    [.Drink:3, .Bird:1])]),
+        MenuCategory(
+            name: "Holiday Meals",
+            range: 5...5,
+            menuItems: [
+                MenuItem(name: "Last Supper", ingredients:
+                    [.Bun:1, .Drink:1]),
+                MenuItem(name: "Good King Wenceslaus", ingredients:
+                    [.Fries:1, .Drink:1, .Cow:1]),
+                MenuItem(name: "Adoration of the Baby Cheeses", ingredients:
+                    [.Cheese:3]),
+                MenuItem(name: "Fourth Day of Christmas", ingredients:
+                    [.Bird:4]),
+                MenuItem(name: "Winter Spending Festival", ingredients:
+                    [.Bun:1, .Fries:1, .Drink:1, .Cheese:1, .Fish:1]),
+                MenuItem(name: "Papa Christmas", ingredients:
+                    [.Bun:1, .Fries:1, .Drink:1, .Cheese:1, .Cow:1, .Pie:1])]),
+        MenuCategory(
+            name: "Special Orders",
+            range: 6...6,
+            menuItems: [
+                MenuItem(name: "Add one Bun", ingredients:
+                    [.Bun:1]),
+                MenuItem(name: "Add one Drink", ingredients:
+                    [.Drink:1]),
+                MenuItem(name: "Add one Fish", ingredients:
+                    [.Fish:1]),
+                MenuItem(name: "Roll two Stuffers", ingredients:
+                    [:]),
+                MenuItem(name: "Roll two Mainstays", ingredients:
+                    [:]),
+                MenuItem(name: "Holiday Potluck", ingredients:
+                    [:])])],
+    smallDeck: Deck(
+        cardCounts: [.Bun:10, .Fries:4, .Drink:8, .Cheese:5, .Cow:4, .Fish:5, .Bird:6, .Pie:1]),
+    largeDeck: Deck(
+        cardCounts: [.Bun:12, .Fries:6, .Drink:12, .Cheese:7, .Cow:6, .Fish:8, .Bird:8, .Pie:1]))]
